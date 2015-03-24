4448052b8590bb8feaa0aeb7c5bcd2bd6f38f2dd
Chris Cotton <chris.cottonn@gmail.com>
2015-03-24 11:31:49 -0700

    #bubble_sort_by method implemented

--- bubble.rb
+++ bubble.rb
@@ -14,4 +14,25 @@ def bubble_sort(array)
     puts "Sorted array: #{array}"
 end
 
+def bubble_sort_by(array)
+    passes = array.length - 1
+    passes.times do 
+        i = 0
+        while i < array.length - 1 do 
+            if yield(array[i], array[i + 1]) < 0
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+            end
+            i += 1    
+        end
+    end
+    puts "Sorted array: #{array}"
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+	right.length - left.length
+end
+
 bubble_sort([4,3,78,2,0,2])
+bubble_sort_by(["hi","hello","hey"])
