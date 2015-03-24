4a8c54ecac55e709a8d74e83e834c82f5feb5eec
Chris Cotton <chris.cottonn@gmail.com>
2015-03-24 11:04:56 -0700

    #bubble_sort method implemented

--- bubble.rb
+++ bubble.rb
@@ -1,5 +1,17 @@
-def bubble_sort
-    
+def bubble_sort(array)
+    passes = array.length - 1
+    passes.times do 
+        i = 0
+        while i < array.length - 1 do 
+            if array[i] > array[i + 1]
+                temp = array[i]
+                array[i] = array[i + 1]
+                array[i + 1] = temp
+            end
+            i += 1    
+        end
+    end
+    puts "Sorted array: #{array}"
 end
 
 bubble_sort([4,3,78,2,0,2])
